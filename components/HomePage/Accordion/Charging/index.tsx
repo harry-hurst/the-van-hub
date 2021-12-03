39becb217f0bbc29fcb13f76795bc82712f51038
harry-hurst <82458413+harry-hurst@users.noreply.github.com>
2021-12-03 20:25:08 +0000

    arrows added to accordion

--- components/HomePage/Accordion/Charging/index.tsx
+++ components/HomePage/Accordion/Charging/index.tsx
@@ -23,6 +23,23 @@ export default function Charging() {
     <motion.div layout id={aboutStyles.container}>
       <motion.h1 layout onClick={toggleOpen} id={aboutStyles.heading}>
         Charging Lithium Ion
+        {" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          stroke="currentColor"
+          strokeWidth="0.5px"
+          id={aboutStyles.arrow}
+          className={`${open && `${aboutStyles.rotate}`} bi bi-chevron-down`}
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
       </motion.h1>
 
       <AnimatePresence>
